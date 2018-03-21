5efd2a598377aa770b31ddd8b5d9993cfbb096c7
Leon Pearce <leonp1991@gmail.com>
2018-03-21 09:30:06 +0800

    UPDATE: modify package to be for a generic amount of artciles

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const API = 'https://hacker-news.firebaseio.com/v0';
 const TOP_ARTICLES_ENDPOINT = `${API}/topstories`;
 const ITEM_ENDPOINT = `${API}/item`;
 
-module.exports = () =>
+module.exports = (count = 30) =>
   fetch(`${TOP_ARTICLES_ENDPOINT}.json`)
     .then(res => res.json())
     .then(json =>
-      Promise.all(json.slice(0, 10).map(articleId =>
+      Promise.all(json.slice(0, count).map(articleId =>
         fetch(`${ITEM_ENDPOINT}/${articleId}.json`)
       )))
     .then(results =>
